ae4cbea0c4e9d54406531f2c1a841faa2cc7dde8
Rudson Kiyoshi <rudson.carvalho@gmail.com>
2017-03-29 23:05:20 -0300

    estudando

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,14 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/***
+ *  Created by rudsonkiyoshi on 27/03/17.
+ *  CrimeActivity controller
+ */
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -10,6 +17,15 @@ public class CrimeActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
 
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 
